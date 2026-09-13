1db57ed10e0ee7cb1d193b26b3fdfb1bf47586b8
168616 <168616@upf.br>
2026-09-13 15:19:43 -0300

    organização do projeto

---
+++ conexão.php
@@ -0,0 +1,20 @@
+<?php
+Function conectaBD() {
+    $Servername = "localhost";
+    $Username = "root";
+    $database = "mercado";
+    $Password = "";
+
+    $conexao = mysqli_connect($Servername, $Username, $Password, $database);
+
+    if(!conexao) {
+        die("Conexão Falhou" . mysqli_connect_error());
+            }
+
+    return $conexao;
+}
+
+Function desconectaBD($conexao) {
+    mysqli_close($conexao);
+}
+?>
